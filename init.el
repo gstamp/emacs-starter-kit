020bb1d73050801e3a53104ec6f3308f98fb3f3b
Glen Stampoultzis <gstamp@gmail.com>
2011-01-19 15:26:55 +1100

    Hiding the menu and toolbar seemed to be causing crashes on my machine

--- init.el
+++ init.el
@@ -11,9 +11,12 @@
 
 ;; Turn off mouse interface early in startup to avoid momentary display
 ;; You really don't need these; trust me.
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+
+;; Commented these out because not having them seems to crash my
+;; ubuntu box.
+;;(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+;;(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+;;(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
 
 ;; Load path etc.
 
