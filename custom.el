fd36f22a6f3c0f3d8b3255ca65911d4d10fadd07
Glen Stampoultzis <gstamp@gmail.com>
2011-07-03 16:35:46 +1000

    Update to ERC defaults

--- custom.el
+++ custom.el
@@ -199,3 +199,15 @@
 ;; (add-hook 'slime-mode-hook 'slime-redirect-inferior-output)
 
 
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(erc-hide-list (quote ("JOIN" "NICK" "QUIT"))))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
